009d3e009ab280f977a0a491e5d15dd1054465a5
Damon Ball <dcball@wpi.edu>
2023-07-07 19:04:46 -0400

    Adding initial enemy movement

--- GMTK 2023/Assets/Scripts/EnemyScript.cs
+++ GMTK 2023/Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private int health;
+    [SerializeField] private int speed;
+
+    private PathPointScript targetPoint = null;
+    private float TARGET_CLOSENESS = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,15 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (targetPoint != null)
+        {
+            move(Time.deltaTime);
+        }
+    }
+
+    public void setPath(PathPointScript startingPoint)
+    {
+        targetPoint = startingPoint;
     }
 
     public void TakeDamage(int damage) {
@@ -24,5 +36,44 @@ public class EnemyScript : MonoBehaviour
             EnemyManager.Instance.RemoveEnemy(gameObject);
             Destroy(gameObject);
         }
+    }
+
+    private void move(float time)
+    {
+        // Sanity checks
+        if (targetPoint.nextPoint == null) return;
+        if (health <= 0) return;
+
+        // Handle moving to next target point, or the end of the path
+        float distanceToTarget = Vector3.Distance(transform.position, targetPoint.transform.position);
+        if (distanceToTarget < TARGET_CLOSENESS)
+        {
+            if (targetPoint.nextPoint == null)
+            {
+                score();
+                targetPoint = null;
+                return;
+            }
+            targetPoint = targetPoint.nextPoint;
+        }
+
+        // Continue walking towards the next path point
+        Vector3 direction = targetPoint.transform.position - transform.position;
+        direction.Normalize();
+
+        // Do not move past the target
+        float movementMagnitude = speed * time;
+        if (movementMagnitude > distanceToTarget)
+        {
+            movementMagnitude = distanceToTarget;
+        }
+
+        transform.position += direction * movementMagnitude;
+        transform.rotation = Quaternion.Euler(direction);
+    }
+
+    private void score()
+    {
+        // TODO - Code for when the enemy reaches the end
     }
 }
