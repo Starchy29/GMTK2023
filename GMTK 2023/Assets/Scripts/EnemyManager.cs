dad1828304f1be33436ef26fb2393dcdb8206151
Starchy29 <ethanball29@gmail.com>
2023-07-07 22:35:32 -0400

    programmed spawning system

--- GMTK 2023/Assets/Scripts/EnemyManager.cs
+++ GMTK 2023/Assets/Scripts/EnemyManager.cs
@@ -6,6 +6,8 @@ public class EnemyManager : MonoBehaviour
 {
     [SerializeField] private int currency;
     [SerializeField] private GameObject commonEnemy;
+    [SerializeField] private GameObject fastEnemy;
+    [SerializeField] private GameObject tankEnemy;
 
     private List<GameObject> enemies;
     public List<GameObject> Enemies { get { return enemies; } }
@@ -13,10 +15,36 @@ public class EnemyManager : MonoBehaviour
     private static EnemyManager instance;
     public static EnemyManager Instance { get { return instance; } }
 
+    private Queue<GameObject> enemyQueue = new Queue<GameObject>();
+    private Queue<GameObject> spawningEnemies = new Queue<GameObject>();
+
+    private const float SECONDS_PER_SPAWN = 0.5f;
+    private const float SPAWN_INTERVAL = 5.0f;
+    private float squadTimer;
+    private float secondsUntilSpwans;
+
     void Awake()
     {
         enemies = new List<GameObject>();
         instance = this;
+    }
+
+    void Update() {
+        secondsUntilSpwans -= Time.deltaTime;
+        if(secondsUntilSpwans <= 0) {
+            secondsUntilSpwans += SPAWN_INTERVAL;
+            spawningEnemies = enemyQueue;
+            enemyQueue = new Queue<GameObject>();
+            squadTimer = 0;
+        }
+
+        if(spawningEnemies.Count > 0) {
+            squadTimer -= Time.deltaTime;
+            if(squadTimer <= 0) {
+                squadTimer += SECONDS_PER_SPAWN;
+                SpawnEnemy(spawningEnemies.Dequeue());
+            }
+        }
     }
 
     public void AddEnemy(GameObject enemy) {
@@ -27,18 +55,35 @@ public class EnemyManager : MonoBehaviour
         enemies.Remove(enemy);
     }
 
-    public void SpawnCommon() {
-        SpawnEnemy(commonEnemy);
+    private void SpawnEnemy(GameObject prefab) {
+        GameObject enemy = Instantiate(prefab);
+        // enemy.transform.position = path start;
     }
 
-    private void SpawnEnemy(GameObject prefab) {
+    private void BuyEnemy(GameObject prefab) {
+        if(enemyQueue.Count >= 5) {
+            return;
+        }
+
         int cost = prefab.GetComponent<EnemyScript>().Cost;
         if(currency < cost) {
             return;
         }
 
         currency -= cost;
-        GameObject enemy = Instantiate(prefab);
-        // enemy.transform.position = path start
+        enemyQueue.Enqueue(prefab);
+    }
+
+    // button functions
+    public void BuyCommon() {
+        BuyEnemy(commonEnemy);
+    }
+
+    public void BuyFast() {
+        BuyEnemy(fastEnemy);
+    }
+
+    public void BuyTank() {
+        BuyEnemy(tankEnemy);
     }
 }
